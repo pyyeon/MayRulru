79b0ccd0a199a6eaa2b21088df521a2b6aae5a0c
seoyeon1221 <rr124356@naver.com>
2024-08-08 14:04:38 +0900

    edit sedyeon

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello rulru");
     }
 }
